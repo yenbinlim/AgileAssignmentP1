fb0cdd98fe36ad10aa60ed4d767ce06fe3e041fc
hongliang97 <44216227+hongliang97@users.noreply.github.com>
2018-11-12 18:18:11 +0800

    As a staff, I want to check the monthly credit limit of the corporate customers so that I can know they allow to make another order or not

--- src/agileassignment/AgileAssignment.java
+++ src/agileassignment/AgileAssignment.java
@@ -28,30 +28,6 @@ public class AgileAssignment {
         CI.setCustName(name);
         System.out.println(CI.getCustInfo());
         
-//        if(category==1){
-//            System.out.println("ID : C0001");
-//            System.out.println("Name : Loh Hong Liang");
-//            System.out.println("Category: Customer");
-//            System.out.println("Gender: M");
-//            System.out.println("Date of Birth: 1997-06-23");
-//            System.out.println("Phone: 012-6427012");
-//            System.out.println("Address: hahahafdajfbasdbfkfbkdsf");
-//            System.out.println("Credit Limit: 0");
-//        }else if(category==2){
-//            System.out.println("ID : C0002");
-//            System.out.println("Name : Cheong Li Jie");
-//            System.out.println("Category: Customer");
-//            System.out.println("Gender: M");
-//            System.out.println("Date of Birth: 1996-10-14");
-//            System.out.println("Phone: 011-1221312");
-//            System.out.println("Address: hahahafdafdfdsjfbasdbfkfbkdsf");
-//            System.out.println("Credit Limit: 2000");
-//            System.out.println("*****This customer not allow to make another purchase before the credit limit did not clear*****");
-//            
-//        }else{
-//            System.out.println("")
-//            
-//        }
     }
     
 }
